b644f3fbbdc99924919e19b4b8a04f8306c31f19
ratna srujana <ratnasrujanavelugu@gmail.com>
2026-07-13 15:09:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
+const dns = require("dns");
+dns.setServer(["8.8.8.8","8.8.8.4.4"])
 const connectDB = require("./config/db");
 const recipeRoutes = require("./routes/recipeRoutes");
 
